burn_in fixed value_network added: reduced variance comes at cost of the slower learning speed config separated; train.py refactored Dockerfile updated

src/agent.py
@@ -31,15 +31,14 @@ class RSAC(nn.Module):
         if training:
             action = dist.sample()
         else:
-            action = dist.sample([1000]).mean(0)
-        action = torch.clamp(action, -utils.ACT_LIM, utils.ACT_LIM)
+            action = dist.sample([100]).mean(0)
         log_prob = dist.log_prob(action)
         return action, log_prob, state
 
     def step(self, obs, actions, rewards, log_probs, hidden_states):
         obs_emb, _ = self.encoder(obs)
         target_obs_emb, _ = self._target_encoder(obs)
-        states = self.cell_roll(self.cell, obs_emb, hidden_states[0], bptt=self._c.bptt)
+        states = self.cell_roll(self.cell, obs_emb, hidden_states[0], burn_in=self._c.burn_in)
         target_states = self.cell_roll(self._target_cell, target_obs_emb, hidden_states[0])
 
         alpha = torch.maximum(self._log_alpha, torch.full_like(self._log_alpha, -20.))
@@ -47,63 +46,76 @@ class RSAC(nn.Module):
 
         rl_loss = self._policy_learning(states, actions, rewards, log_probs, target_states, alpha.detach())
         auxiliary_loss = self._auxiliary_loss(obs, actions, states, target_states)
-        actor_loss, dual_loss = self._policy_improvement(target_states, alpha)
+        actor_loss, dual_loss = self._policy_improvement(states.detach(), alpha)
         model_loss = rl_loss + auxiliary_loss + actor_loss + dual_loss
 
         self.optim.zero_grad()
         model_loss.backward()
         clip_grad_norm_(self.parameters(), self._c.max_grad)
         self.optim.step()
-        #self.callback.add_scalar('train/actor_loss', actor_loss.item(), self._step)
+        self.callback.add_scalar('train/actor_loss', actor_loss.item(), self._step)
         self.callback.add_scalar('train/auxiliary_loss', auxiliary_loss.item(), self._step)
-        #self.callback.add_scalar('train/critic_loss', rl_loss.item(), self._step)
-        # self.callback.add_scalar('train/actor_grads', utils.grads_sum(self.actor), self._step)
-        # self.callback.add_scalar('train/critic_grads', utils.grads_sum(self.critic), self._step)
-        # self.callback.add_scalar('train/encoder_grads', utils.grads_sum(self.encoder), self._step)
-        # self.callback.add_scalar('train/cell_grads', utils.grads_sum(self.cell), self._step)
+        self.callback.add_scalar('train/critic_loss', rl_loss.item(), self._step)
+        self.callback.add_scalar('train/actor_grads', utils.grads_sum(self.actor), self._step)
+        self.callback.add_scalar('train/critic_grads', utils.grads_sum(self.critic), self._step)
+        self.callback.add_scalar('train/encoder_grads', utils.grads_sum(self.encoder), self._step)
+        self.callback.add_scalar('train/cell_grads', utils.grads_sum(self.cell), self._step)
         self._update_targets()
         self._step += 1
 
     def _policy_learning(self, states, actions, rewards, behaviour_log_probs, target_states, alpha):
+        truncate = lambda t: t[:-1]
         with torch.no_grad():
             target_dist = self._target_actor(target_states)
             sampled_actions = target_dist.sample([self._c.num_samples])
             sampled_log_probs = target_dist.log_prob(sampled_actions).unsqueeze(-1)
-            values = self._target_critic(torch.repeat_interleave(target_states[None], self._c.num_samples, 0),
-                                         sampled_actions).min(-1, keepdim=True).values
-            soft_values = torch.mean(values - alpha*sampled_log_probs, 0)
-            target_values = self._target_critic(target_states, actions).min(-1, keepdim=True).values
+            q_values = self._target_critic(
+                torch.repeat_interleave(target_states[None], self._c.num_samples, 0),
+                sampled_actions
+            ).min(-1, keepdim=True).values
+            target_soft_values = torch.mean(q_values - alpha*sampled_log_probs, 0)
+            target_q_values = self._target_critic(target_states, actions).min(-1, keepdim=True).values
+            soft_values = self._target_value(target_states)
+            
             log_probs = target_dist.log_prob(actions).unsqueeze(-1)
             cs = torch.minimum(torch.tensor(1.), (log_probs - behaviour_log_probs).exp())
-            deltas = rewards + self._c.discount*soft_values.roll(-1, 0) - target_values
-            target_values, deltas, cs = map(lambda t: t[:-1], (target_values, deltas, cs))
-            target_values = utils.retrace(target_values, deltas, cs, self._c.discount, self._c.disclam)
+            deltas = rewards + self._c.discount*soft_values.roll(-1, 0) - target_q_values
+            target_q_values, deltas, cs = map(truncate, (target_q_values, deltas, cs))
+            deltas = utils.retrace(deltas, cs, self._c.discount, self._c.disclam)
+            target_q_values += deltas
 
         q_values = self.critic(states, actions)
-        loss = (q_values[:-1] - target_values).pow(2)
+        values = self.value(states.detach())
+        q_values, values, target_soft_values = map(truncate, (q_values, values, target_soft_values))
+        loss = (q_values - target_q_values).pow(2) + (values - target_soft_values).pow(2)
+        loss = self._sequence_discount(loss)*loss
 
-        self.callback.add_scalar('train/mean_reward', rewards.mean().item(), self._step)
+        self.callback.add_scalar('train/mean_reward', rewards.mean().item() / self._c.action_repeat, self._step)
         self.callback.add_scalar('train/mean_value', q_values.mean().item(), self._step)
-        # self.callback.add_scalar('train/retrace_weight', cs.mean(), self._step)
-        # self.callback.add_scalar('train/mean_deltas', deltas.mean(), self._step)
+        self.callback.add_scalar('train/retrace_weight', cs.mean(), self._step)
+        self.callback.add_scalar('train/mean_deltas', deltas.mean(), self._step)
         return loss.mean()
 
     def _policy_improvement(self, states, alpha):
         dist = self.actor(states)
         actions = dist.rsample([self._c.num_samples])
         log_prob = dist.log_prob(actions)
-        q_values = self._target_critic(torch.repeat_interleave(states[None], self._c.num_samples, 0),
-                                       actions).min(-1).values
+        self.critic.requires_grad_(False)
+        q_values = self.critic(
+            torch.repeat_interleave(states[None], self._c.num_samples, 0),
+            actions
+        ).min(-1).values
+        self.critic.requires_grad_(True)
 
         with torch.no_grad():
             ent = -log_prob.mean()
             self.callback.add_scalar('train/actor_entropy', ent.item(), self._step)
             self.callback.add_scalar('train/alpha', alpha.item(), self._step)
-
+        
         actor_loss = torch.mean(alpha.detach() * log_prob - q_values, 0)
-        actor_loss = self._masked_discount(actor_loss, self._c.burn_in) * actor_loss
-        dual_loss = - alpha * (log_prob.detach() + self._target_entropy)
-        return actor_loss.mean(), dual_loss.mean()
+        actor_loss = self._sequence_discount(actor_loss)*actor_loss
+        dual_loss = - alpha * (log_prob.detach().mean() + self._target_entropy)
+        return actor_loss.mean(), dual_loss
 
     def _auxiliary_loss(self, obs, actions, states_emb, target_states_emb):
         # todo check l2 reg
@@ -117,7 +129,6 @@ class RSAC(nn.Module):
                 loss = (obs_pred - obs).pow(2)
             return loss.mean()
         elif self._c.aux_loss == 'contrastive':
-            #import pdb; pdb.set_trace()
             actions = actions[:-1].unfold(0, self._c.spr_depth, 1).flatten(0, 1).movedim(-1, 0)
             states_emb = states_emb[:-self._c.spr_depth].flatten(0, 1)
 
@@ -135,23 +146,19 @@ class RSAC(nn.Module):
             raise NotImplementedError
 
     @staticmethod
-    def cell_roll(cell, inp, state, bptt=-1):
+    def cell_roll(cell, inp, state, burn_in=-1):
         states = []
         for i, x in enumerate(inp):
-            if bptt > 0 and i % bptt == 0:
-                state = state.detach()
             state = cell(x, state)
+            if i < burn_in:
+                state = state.detach()
             states.append(state)
         return torch.stack(states)
 
-    def _masked_discount(self, x, size=-1):
-        size = min(max(size, 0), x.size(0))
-        mask = torch.cat([torch.zeros(size, device=self.device), torch.ones(x.size(0) - size, device=self.device)])
+    def _sequence_discount(self, x):
         discount = self._c.discount ** torch.arange(x.size(0), device=self.device)
-        mask = mask * discount
-        while mask.ndimension() != x.ndimension():
-            mask = mask.unsqueeze(-1)
-        return mask
+        shape = (x.ndimension() - 1) * (1,)
+        return discount.reshape(-1, *shape)
 
     def _build(self):
         # TODO: weight decay, value-function, make contrastive dimensions larger
@@ -161,15 +168,15 @@ class RSAC(nn.Module):
         # RL
         self.cell = nn.GRUCell(emb, hidden)
         self.actor = models.Actor(hidden, self.act_dim, layers=self._c.actor_layers,
-                                  mean_scale=self._c.mean_scale, init_std=self._c.init_std)
+                                  mean_scale=self._c.mean_scale)
 
-        self.value = utils.build_mlp((hidden, *self._c.critic_layers, 1))
+        self.value = utils.build_mlp(hidden, *self._c.actor_layers, 1)
         self.critic = models.Critic(hidden + self.act_dim, self._c.critic_layers)
 
         # SPR
         self.dm = nn.GRUCell(self.act_dim, hidden)
-        self.projection = utils.build_mlp((hidden, emb, emb))
-        self.prediction = utils.build_mlp((emb, emb, emb))
+        self.projection = utils.build_mlp(hidden, emb, emb)
+        self.prediction = utils.build_mlp(emb, emb, emb)
         self.cos_sim = nn.CosineSimilarity(dim=-1)
 
         obs_dim = self.obs_spec.shape[0]
@@ -190,21 +197,17 @@ class RSAC(nn.Module):
 
         self._target_encoder, self._target_actor, self._target_value,\
             self._target_critic, self._target_cell, self._target_projection =\
-            map(lambda m: deepcopy(m).requires_grad_(False),
-                (self.encoder, self.actor, self.value, self.critic, self.cell, self.projection))
+            utils.make_targets(self.encoder, self.actor, self.value, self.critic, self.cell, self.projection)
 
-        def param_group(*modules):
-            return nn.ParameterList(chain(*map(nn.Module.parameters, modules)))
-        self._rl_params = param_group(self.cell, self.critic, self.value)
-        self._ae_params = param_group(self.encoder, self.dm, self.projection, self.prediction, self.decoder)
+        self._rl_params = utils.make_param_group(self.cell, self.critic, self.value, self.actor)
+        self._ae_params = utils.make_param_group(self.encoder, self.dm, self.projection, self.prediction, self.decoder)
 
         self.optim = torch.optim.Adam([
-            {'params': self.actor.parameters(), 'lr': self._c.actor_lr},
-            {'params': self._rl_params, 'lr': self._c.critic_lr},
-            {'params': self._ae_params, 'lr': self._c.encoder_lr, 'weight_decay': self._c.weight_decay},
+            {'params': self._rl_params, 'lr': self._c.rl_lr},
+            {'params': self._ae_params, 'lr': self._c.ae_lr, 'weight_decay': self._c.weight_decay},
             {'params': [self._log_alpha], 'lr': self._c.dual_lr}
         ])
-        self._target_entropy = -self.act_dim
+        self._target_entropy = - 3.*self.act_dim
         self.to(self.device)
 
     @torch.no_grad()
@@ -213,4 +216,5 @@ class RSAC(nn.Module):
         utils.soft_update(self._target_projection, self.projection, self._c.encoder_tau)
         utils.soft_update(self._target_cell, self.cell, self._c.critic_tau)
         utils.soft_update(self._target_critic, self.critic, self._c.critic_tau)
+        utils.soft_update(self._target_value, self.value, self._c.critic_tau)
         utils.soft_update(self._target_actor, self.actor, self._c.actor_tau)

src/core.py
@@ -1,4 +1,3 @@
-from dataclasses import dataclass, field
 from .agent import RSAC
 from . import wrappers, utils
 import torch
@@ -8,59 +7,8 @@ import pathlib
 import datetime
 from collections import deque
 import numpy as np
-import pdb
-
+import pickle
 torch.autograd.set_detect_anomaly(True)
-
-
-@dataclass
-class Config(utils.BaseConfig):
-    discount: float = .99
-    disclam: float = 1.
-    num_samples: int = 16
-    action_repeat: int = 2
-
-    critic_layers: tuple = (256, 256)
-    actor_layers: tuple = (256, 256)
-    hidden_dim: int = 256
-    obs_emb_dim: int = 64
-    init_log_alpha: float = 1.
-    init_std: float = 2.
-    mean_scale: float = 5.
-    spr_coef: float = 2.
-    spr_depth: int = 5
-
-    critic_lr: float = 1e-3
-    actor_lr: float = 1e-3
-    dual_lr: float = 1e-2
-    encoder_lr: float = 1e-3
-    weight_decay: float = 1e-7
-
-    critic_tau: float = .995
-    actor_tau: float = .995
-    encoder_tau: float = .995
-
-    total_steps: int = 2 * 10 ** 6
-    training_steps: int = 200
-    seq_len: int = 50
-    eval_freq: int = 20000
-    max_grad: float = 100.
-    batch_size: int = 50
-    buffer_size: int = 1000
-    burn_in: int = -1
-    bptt: int = -1
-
-
-    # PointNet
-    pn_number: int = 600
-    pn_layers: tuple = (32, 64, 128)
-    pn_dropout: float = 0.
-
-    task: str = 'walker_stand'
-    aux_loss: str = 'None'
-    logdir: str = 'logdir'
-    device: str = 'cuda'
-    observe: str = 'point_cloud'
 
 
 class RLAlg:
@@ -110,19 +58,25 @@ class RLAlg:
     def save(self):
         torch.save({
             'interactions': self.interactions_count,
-            'agent': self.agent.state_dict(),
+            'params': self.agent.state_dict(),
             'optim': self.agent.optim.state_dict(),
         }, self._task_path / 'checkpoint')
+        with open(self._task_path / 'buffer', 'wb') as b:
+            pickle.dump(self.buffer, b)
 
     def load(self, path):
         path = pathlib.Path(path)
-        self.config = self.config.load(path / 'config.yml')
+        [f.unlink() for f in path.iterdir() if f.match('*tfevents*')]
+        self.config = self.config.load(path / 'config.yml', **kwargs)
         if (path / 'checkpoint').exists():
             chkp = torch.load(path / 'checkpoint')
             with torch.no_grad():
-                self.agent.load_state_dict(chkp['agent'])
+                self.agent.load_state_dict(chkp['params'])
                 self.agent.optim.load_state_dict(chkp['optim'])
             self.interactions_count = chkp['interactions']
+            
+        with open(self._task_path / 'buffer', 'rb') as b:
+            self.buffer = pickle.load(b)
 
     def _make_env(self):
         env = utils.make_env(self.config.task)

src/models.py
@@ -9,7 +9,7 @@ td = torch.distributions
 class Critic(nn.Module):
     def __init__(self, in_features, layers):
         super().__init__()
-        self.qs = nn.ModuleList([build_mlp((in_features, *layers, 1)) for _ in range(2)])
+        self.qs = nn.ModuleList([build_mlp(in_features, *layers, 1) for _ in range(2)])
 
     def forward(self, obs, action):
         x = torch.cat([obs, action], -1)
@@ -18,10 +18,10 @@ class Critic(nn.Module):
 
 
 class Actor(nn.Module):
-    def __init__(self, in_features, out_features, layers, mean_scale=1, init_std=2.):
+    def __init__(self, in_features, out_features, layers, mean_scale=1, init_std=1.):
         super().__init__()
         self.mean_scale = mean_scale
-        self.mlp = build_mlp((in_features, *layers, 2*out_features))
+        self.mlp = build_mlp(in_features, *layers, 2*out_features)
         self.init_std = torch.log(torch.tensor(init_std).exp() - 1.)
 
     def forward(self, x):

src/utils.py
@@ -1,20 +1,17 @@
+import copy
 import torch
-import numpy as np
-from collections import deque
 import random
+import numpy as np
+from itertools import chain
+from collections import deque
 from torch.utils.data import Dataset
 from dm_control import suite, manipulation
-import math
-import dataclasses
-from abc import abstractmethod, ABC
-from ruamel.yaml import YAML
 nn = torch.nn
 F = nn.functional
 td = torch.distributions
-ACT_LIM = .9997
 
 
-def build_mlp(sizes, act=nn.ELU):
+def build_mlp(*sizes, act=nn.ELU):
     mlp = []
     for i in range(1, len(sizes)):
         mlp.append(nn.Linear(sizes[i-1], sizes[i]))
@@ -26,8 +23,8 @@ def grads_sum(model):
     s = 0
     for p in model.parameters():
         if p.grad is not None:
-            s += p.grad.sum().item()
-    return s
+            s += p.grad.pow(2).sum().item()
+    return np.sqrt(s)
 
 
 def make_env(name, **kwargs):
@@ -44,18 +41,20 @@ def simulate(env, policy, training):
     # done flags might be useful for another learning alg
     obs = env.reset()
     done = False
-    prev_state = None
-    observations, actions, rewards, log_probs, states = [], [], [], [], []  # states=recurrent hidden
+    state = None
+    observations, actions, rewards, dones, log_probs, states = [[] for _ in range(6)]  # states=recurrent hidden
+    detach = lambda t: t.detach().cpu().flatten().numpy()
     while not done:
-        if torch.is_tensor(prev_state):
-            states.append(prev_state.detach().cpu().flatten().numpy())
-            action, log_prob, prev_state = policy(obs, prev_state, training)
+        if torch.is_tensor(state):
+            states.append(detach(state))
+            action, log_prob, state = policy(obs, state, training)
         else:
-            action, log_prob, prev_state = policy(obs, prev_state, training)
-            states.append(torch.zeros_like(prev_state).detach().cpu().flatten().numpy())
+            action, log_prob, state = policy(obs, state, training)
+            states.append(detach(torch.zeros_like(state)))
         new_obs, reward, done = env.step(action)
         observations.append(obs)
         actions.append(action)
+        dones.append([done])
         rewards.append([reward])
         log_probs.append(log_prob)
         obs = new_obs
@@ -64,6 +63,7 @@ def simulate(env, policy, training):
         observations=observations,
         actions=actions,
         rewards=rewards,
+        done_flags=dones,
         states=states,
         log_probs=log_probs,
     )
@@ -92,7 +92,7 @@ class TrajectoryBuffer(Dataset):
 
 
 class TanhTransform(td.transforms.TanhTransform):
-    lim = ACT_LIM
+    lim = .99999997
     
     def _inverse(self, y):
         y = torch.clamp(y, min=-self.lim, max=self.lim)
@@ -104,17 +104,8 @@ def soft_update(target, online, rho):
         pt.copy_(rho * pt + (1. - rho) * po)
 
 
-def gve(rewards, next_values, discount, disclam):
-    target_values = []
-    last_val = next_values[-1]
-    for r, v in zip(rewards.flip(0), next_values.flip(0)):
-        last_val = r + discount*(disclam*last_val + (1.-disclam)*v)
-        target_values.append(last_val)
-    return torch.stack(target_values).flip(0)
-
-
-def retrace(values, resids, cs, discount, disclam):
-    cs = torch.cat((cs[1:], torch.ones_like(cs[0])[None]))
+def retrace(resids, cs, discount, disclam):
+    cs = torch.cat((cs[1:], torch.ones_like(cs[-1:])))
     cs *= disclam
     resids, cs = map(lambda t: t.flip(0), (resids, cs))
     deltas = []
@@ -122,24 +113,12 @@ def retrace(values, resids, cs, discount, disclam):
     for r, c in zip(resids, cs):
         last_val = r + last_val * discount * c
         deltas.append(last_val)
-    return values + torch.stack(deltas).flip(0)
+    return torch.stack(deltas).flip(0)
 
 
-@dataclasses.dataclass
-class BaseConfig(ABC):
-    def save(self, file_path):
-        yaml = YAML()
-        with open(file_path, 'w') as f:
-            yaml.dump(dataclasses.asdict(self), f)
+def make_param_group(*modules):
+    return nn.ParameterList(chain(*map(nn.Module.parameters, modules)))
 
-    def load(self, file_path):
-        yaml = YAML()
-        with open(file_path) as f:
-            config_dict = yaml.load(f)
-        return dataclasses.replace(self, **config_dict)
 
-    def __post_init__(self):
-        for field in dataclasses.fields(self):
-            value = getattr(self, field.name)
-            value = field.type(value)
-            setattr(self, field.name, value)
+def make_targets(*modules):
+    return map(lambda m: copy.deepcopy(m).requires_grad_(False), modules)