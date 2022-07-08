46a419b7cf5d25c630c22fc35bc5c1198d448537
Leonardo130709 <78939351+Leonardo130709@users.noreply.github.com>
2022-07-08 06:16:59 +0300

    pca changed

--- src/pca.py
+++ src/pca.py
@@ -32,14 +32,15 @@ class PCA(nn.Module):
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('path', help='Path to directory containing weights and config.')
-    parser.add_argument('--lr', type=float, default=1e-3)
-    parser.add_argument('--epochs', type=int, default=100)
-    parser.add_argument('--batch_size', type=int, default=2)
+    parser.add_argument('--lr', type=float, default=1e-2)
+    parser.add_argument('--epochs', type=int, default=40)
+    parser.add_argument('--batch_size', type=int, default=5)
     return parser.parse_args()
 
 
 def train_pca(path, lr, epochs, batch_size):
     alg = RLAlg.load(path)
+    alg.agent.requires_grad_(False)
     pca = PCA(alg, lr)
     monitor = wrappers.Monitor(alg.env)
 
@@ -65,10 +66,12 @@ def train_pca(path, lr, epochs, batch_size):
                               )
     for i in range(states_pred.shape[-1]):
         plt.figure(figsize=(10, 6))
+        plt.title(f'proprietary state[{i}]')
         plt.plot(states_pred[:, i], label='pred')
         plt.plot(states[:, i], label='truth')
+        plt.xlabel('timestep')
         plt.legend()
-        plt.show()
+        plt.savefig(f'coord{i}.pdf', format='pdf')
 
     return pca
 
